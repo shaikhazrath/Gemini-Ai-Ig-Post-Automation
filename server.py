53f72d35c890cd0f03eeb237b3605f3383314db4
shaikhazrath <shaikhazrathali123@gmail.com>
2024-05-06 22:03:10 +0530

    first commit

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     create_quote_post()
-    return "Hello, welcome to my Flask server!"
+    return "posted"
 
 if __name__ == '__main__':
     app.run(debug=True)
